3d6d5fa2aeccf928779691076987ddd4f5db0450
Hyunggi Chang <hyunggi.chang95@gmail.com>
2022-02-14 13:58:55 +0900

    support MacOS

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ parser.add_argument('--setup', action='store_true',
                     help='Set this flag if running for the first time')
 args = parser.parse_args()
 
-if os.name == 'posix':
+if os.name == 'posix' or os.name == 'darwin':
     if args.setup:
         os.system("chmod u+x ./scripts/setup.sh")
         os.system("./scripts/setup.sh")
